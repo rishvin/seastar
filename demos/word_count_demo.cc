c7e8fdb37dee99463694d3d9a5b93fc153ccd95b
root <rishab99999@gmail.com>
2023-08-06 16:05:13 +0000

    Refactored code to tasks.

--- demos/word_count_demo.cc
+++ demos/word_count_demo.cc
@@ -36,14 +36,65 @@ using WordCountMap = std::unordered_map<std::string, size_t>;
 using WordCountMapPtr = std::unique_ptr<WordCountMap>;
 using ShouldReadBufferFunc = std::function<bool(const char *, size_t)>;
 
-class WordCount {
+class FileMapReduceTask {
 public:
-  WordCount(seastar::file fileDesc, size_t startOffset,
-            size_t estimatedBytesToRead)
-      : _fileDesc{std::move(fileDesc)}, _fileOffset{startOffset},
-        _estimatedBytesToRead{estimatedBytesToRead} {}
+  virtual ~FileMapReduceTask() = default;
+  virtual void map(const char &ch) = 0;
+  virtual void onMapComplete() = 0;
+  virtual void reduce(const FileMapReduceTask &other) = 0;
+};
 
-  seastar::future<WordCountMapPtr> process() {
+using FileMapReduceTaskPtr = std::unique_ptr<FileMapReduceTask>;
+using FileMapReduceTaskFactoryFunc = std::function<FileMapReduceTaskPtr()>;
+
+class WordCountTask : public FileMapReduceTask {
+public:
+  void map(const char &ch) final {
+    static constexpr const char *kDelimiters = " \n.,:;";
+    if (std::strchr(kDelimiters, ch) == nullptr) {
+      _word += ch;
+      return;
+    }
+
+    if (!_word.empty()) {
+      (*_wordCountMap)[_word]++;
+      _word.clear();
+    }
+  }
+
+  void onMapComplete() final {
+    logger.info("Total words: {}", _wordCountMap->size());
+  }
+
+  void reduce(const FileMapReduceTask &otherFileMapReduceTask) final {
+    const auto &otherWordCountMap =
+        static_cast<const WordCountTask &>(otherFileMapReduceTask)
+            ._wordCountMap;
+
+    for (const auto &[word, count] : *otherWordCountMap) {
+      (*_wordCountMap)[word] += count;
+    }
+  }
+
+  static FileMapReduceTaskPtr create() {
+    return std::make_unique<WordCountTask>();
+  }
+
+private:
+  std::string _word;
+  WordCountMapPtr _wordCountMap = std::make_unique<WordCountMap>();
+};
+
+class FileMapReduceProcessor {
+public:
+  FileMapReduceProcessor(seastar::file &&fileDesc, size_t startOffset,
+                         size_t estimatedBytesToRead,
+                         FileMapReduceTaskPtr &&mapReduceTask)
+      : _fileDesc{std::move(fileDesc)}, _fileOffset{startOffset},
+        _estimatedBytesToRead{estimatedBytesToRead}, _mapReduceTask{std::move(
+                                                         mapReduceTask)} {}
+
+  seastar::future<FileMapReduceTaskPtr> process() {
     return seastar::repeat([this] {
              return seastar::do_with(
                  seastar::allocate_aligned_buffer<char>(kBufferSize,
@@ -52,40 +103,41 @@ public:
                    return _fileDesc
                        .dma_read(_fileOffset, buffer.get(), kBufferSize)
                        .then([this, &buffer](size_t bytesRead) {
-                         if (bytesRead == 0) {
-                           return seastar::stop_iteration::yes;
-                         }
-
-                         auto bufferOffset =
-                             _getBufferStartOffset(buffer.get(), bytesRead);
-
-                         if (_totalBytesRead < _estimatedBytesToRead) {
-                           std::tie(_isComplete, bufferOffset) =
-                               _populateWordCount(buffer.get(), bytesRead,
-                                                  bufferOffset);
-                         }
-
-                         if (!_isComplete &&
-                             _totalBytesRead >= _estimatedBytesToRead) {
-                           std::tie(_isComplete, bufferOffset) =
-                               _populateRemaining(buffer.get(), bytesRead,
-                                                  bufferOffset);
-                         }
-
-                         _fileOffset += kBufferSize;
-
-                         return _isComplete ? seastar::stop_iteration::yes
-                                            : seastar::stop_iteration::no;
+                         return _processBuffer(buffer.get(), bytesRead)
+                                    ? seastar::stop_iteration::yes
+                                    : seastar::stop_iteration::no;
                        });
                  });
            })
         .then([this] {
-          return seastar::make_ready_future<WordCountMapPtr>(
-              std::move(_wordCountMap));
+          return seastar::make_ready_future<FileMapReduceTaskPtr>(
+              std::move(_mapReduceTask));
         });
   }
 
 private:
+  bool _processBuffer(const char *buffer, const size_t &bufferSize) {
+    if (bufferSize == 0) {
+      return true;
+    }
+
+    auto isComplete = false;
+    auto bufferOffset = _getBufferStartOffset(buffer, bufferSize);
+
+    if (_totalBytesRead < _estimatedBytesToRead) {
+      std::tie(isComplete, bufferOffset) =
+          _populateWordCount(buffer, bufferSize, bufferOffset);
+    }
+
+    if (!isComplete && _totalBytesRead >= _estimatedBytesToRead) {
+      std::tie(isComplete, bufferOffset) =
+          _populateRemaining(buffer, bufferSize, bufferOffset);
+    }
+
+    _fileOffset += kBufferSize;
+    return isComplete;
+  }
+
   size_t _getBufferStartOffset(const char *buffer, const size_t &bufferSize) {
     if (_fileOffset == 0 || _totalBytesRead > 0) {
       return 0;
@@ -105,7 +157,7 @@ private:
     for (; offset < bufferSize && _totalBytesRead < _estimatedBytesToRead;
          offset++) {
       ch = buffer[offset];
-      _maybeInsertWord(ch, _word);
+      _executeMap(ch);
     }
 
     return {ch == '\n', offset};
@@ -117,7 +169,7 @@ private:
     bool isComplete = false;
     for (; offset < bufferSize; offset++) {
       const char ch = buffer[offset];
-      _maybeInsertWord(ch, _word);
+      _executeMap(ch);
 
       if (ch == '\n') {
         isComplete = true;
@@ -128,19 +180,9 @@ private:
     return {isComplete, offset};
   }
 
-  void _maybeInsertWord(const char &ch, std::string &word) {
+  void _executeMap(const char &ch) {
     _totalBytesRead++;
-
-    if (ch != ' ' && ch != '\n' && ch != '.' && ch != ',' && ch != ':' &&
-        ch != ';') {
-      word += ch;
-      return;
-    }
-
-    if (!word.empty()) {
-      (*_wordCountMap)[word]++;
-      word.clear();
-    }
+    _mapReduceTask->map(ch);
   }
 
 public:
@@ -149,16 +191,14 @@ public:
   size_t _fileOffset;
   const size_t _estimatedBytesToRead;
   size_t _totalBytesRead = 0;
-  bool _isComplete = false;
 
-  std::string _word;
-
-  WordCountMapPtr _wordCountMap = std::make_unique<WordCountMap>();
+  FileMapReduceTaskPtr _mapReduceTask;
 };
 
-class ParallelFileProcessor {
+class FileMapReduce {
 public:
-  ParallelFileProcessor() {
+  FileMapReduce(FileMapReduceTaskFactoryFunc &&mapReduceTaskFactoryFunc)
+      : _mapReduceTaskFactoryFunc{std::move(mapReduceTaskFactoryFunc)} {
     _app.add_options()("file_path",
                        boost::program_options::value<std::string>()->required(),
                        "Path to file to count words in");
@@ -203,12 +243,14 @@ private:
                                           const size_t &fileSize, size_t offset,
                                           size_t bytesToRead) {
     return seastar::open_file_dma(filePath, seastar::open_flags::ro)
-        .then([fileSize, offset, bytesToRead](seastar::file fileDesc) {
+        .then([this, fileSize, offset, bytesToRead](seastar::file fileDesc) {
+          auto fileProcessor =
+              FileMapReduceProcessor(std::move(fileDesc), offset, bytesToRead,
+                                     _mapReduceTaskFactoryFunc());
           return seastar::do_with(
-              WordCount(std::move(fileDesc), offset, bytesToRead),
-              [](WordCount &wordCount) {
-                return wordCount.process().then([](auto &&wordCountMap) {
-                  logger.info("{}", wordCountMap->size());
+              std::move(fileProcessor), [](auto &fileProcessor) {
+                return fileProcessor.process().then([](auto &&mapReduceTask) {
+                  mapReduceTask->onMapComplete();
                 });
               });
         });
@@ -216,12 +258,27 @@ private:
 
   static constexpr const char *kFilePathKey = "file_path";
   seastar::app_template _app;
+
+  FileMapReduceTaskFactoryFunc _mapReduceTaskFactoryFunc;
 };
 
 } // namespace
 
 int main(int argc, char **argv) {
   seastar::app_template app;
-  ParallelFileProcessor processor;
+  FileMapReduce processor(WordCountTask::create);
+
+  // return app.run(argc, argv, [] {
+  //   std::vector<int> chunks = {1, 2, 3, 4};
+  //   auto mapper = [](auto &c) { return seastar::make_ready_future<int>(c); };
+  //   return seastar::map_reduce(
+  //              chunks.begin(), chunks.end(), mapper, 0,
+  //              [](const int &c1, const int &c2) { return c1 + c2; })
+  //       .then([](auto result) {
+  //         std::cout << result << std::endl;
+  //         return seastar::make_ready_future<>();
+  //       });
+  // });
+
   return processor.run(argc, argv);
 }
